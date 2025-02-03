638113d62826de586bca25b2e790b854f991eba9
rohitisog <rohitisog@gmail.com>
2025-02-04 00:58:43 +0530

    Open Source Section Donw

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import "./App.css"
+import OpenSource from './components/OpenSource'
 
 const App = () => {
   return (
     <>
     <Header/>
     <Hero/>
+    <OpenSource/>
     </>
   )
 }
